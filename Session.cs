93eb605d6ca26bdadd1eaf7663dd47d11fb96757
Artikman <47893341+Artikman@users.noreply.github.com>
2019-02-23 01:03:47 +0300

    Create Session.cs

---
+++ Session.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Lab1.Model
+{
+    public class Session
+    {
+        public int SessionId { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime TimeStarted { get; set; }
+        public DateTime EndTime { get; set; }
+        public float TicketPrice { get; set; }
+        public string EmployessInvolvedInTheSession { get; set; }
+        public int? PlaceId { get; set; }
+        public Place Places { get; set; }
+        public virtual ICollection<Film> Films { get; set; }
+    }
+}
